f1ffae9c9f48928b7afcfb994b1dce175fe2653c
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-12-28 21:26:47 -0500

    (fix) inputs not setting state properly
    -> handleInput had to be refactored, due to the cvData (data.jsx) structure change the function. It sets states depending of a case in the switch statement.
    If the setState is education or experience it copies everything up until the children key from the object and sets its value to call the function updateChildrenValues.
    
    (feat) function helper called updateChildrenValues that receives an array, a id and a value and returns a updated version of that received array.
      - more specifically it goes through each element from that array with map(), whenever it reaches the last element it copies the element and changes a value from that object.
    
    (Note) - checking if it works when two groups of inputs were added and the first one is filled first.

--- src/components/cv-application.jsx
+++ src/components/cv-application.jsx
@@ -5,99 +5,141 @@ import Experience from './experience';
 import Button from './buttons';
 
 function CvApplication({ inputValues, setInputValues, setPage }) {
-	const elementsInfo = dataElementGeneration;
+  const elementsInfo = dataElementGeneration;
 
-	function handleInput(e) {
-		const id = e.target.id;
-		const value = e.target.value;
-		setInputValues({ ...inputValues, [id]: value });
-	}
+  function handleInput(e) {
+    const className = e.target.className;
+    const id = e.target.id;
+    const value = e.target.value;
 
-	function handleAddInputs(e) {
-		const className = e.target.className;
+    switch (className) {
+      case 'personalInfo': {
+        const personalInfo = inputValues.personalInfo;
+        setInputValues({
+          ...inputValues,
+          personalInfo: { ...personalInfo, [id]: value },
+        });
+        break;
+      }
+      case 'education': {
+        const education = inputValues.education;
+        const eduChildren = education.children;
+        setInputValues({
+          ...inputValues,
+          education: {
+            ...education,
+            children: updateChildrenValues(eduChildren, id, value),
+          },
+        });
+        break;
+      }
+      case 'experience': {
+        const experience = inputValues.education;
+        const expChildren = experience.children;
+        setInputValues({
+          ...inputValues,
+          experience: {
+            ...experience,
+            children: updateChildrenValues(expChildren, id, value),
+          },
+        });
+        break;
+      }
+    }
+  }
 
-		if (className === 'education') {
-			const education = inputValues.education;
-			const children = inputValues.education.children;
-			setInputValues({
-				...inputValues,
-				education: {
-					...education,
-					children: [
-						...children,
-						{
-							id: crypto.randomUUID(),
-							schoolName: '',
-							studyCertification: '',
-							studyDate: '',
-						},
-					],
-				},
-			});
-		} else {
-			const experience = inputValues.experience;
-			const children = inputValues.experience.children;
-			setInputValues({
-				...inputValues,
-				experience: {
-					...experience,
-					children: [
-						...children,
-						{
-							id: crypto.randomUUID(),
-							schoolName: '',
-							studyCertification: '',
-							studyDate: '',
-						},
-					],
-				},
-			});
-		}
-	}
+  function handleAddInputs(e) {
+    const className = e.target.className;
 
-	function handleSubmit(e) {
-		e.preventDefault();
-		setPage(1);
-	}
+    if (className === 'education') {
+      const education = inputValues.education;
+      const children = inputValues.education.children;
+      setInputValues({
+        ...inputValues,
+        education: {
+          ...education,
+          children: [
+            ...children,
+            {
+              id: crypto.randomUUID(),
+              schoolName: '',
+              studyCertification: '',
+              studyDate: '',
+            },
+          ],
+        },
+      });
+    } else {
+      const experience = inputValues.experience;
+      const children = inputValues.experience.children;
+      setInputValues({
+        ...inputValues,
+        experience: {
+          ...experience,
+          children: [
+            ...children,
+            {
+              id: crypto.randomUUID(),
+              schoolName: '',
+              studyCertification: '',
+              studyDate: '',
+            },
+          ],
+        },
+      });
+    }
+  }
 
-	return (
-		<>
-			<Header className="title" titleText="CV Application" />
-			<main>
-				<form className="application">
-					<PersonalInformation
-						dataInfo={elementsInfo.generalInfo}
-						onChange={handleInput}
-						inputValues={inputValues}
-						onClick={handleAddInputs}
-					/>
-					<Education
-						dataInfo={elementsInfo.education}
-						onChange={handleInput}
-						inputValues={inputValues}
-						onClick={handleAddInputs}
-					/>
-					<Experience
-						dataInfo={elementsInfo.work}
-						onChange={handleInput}
-						inputValues={inputValues}
-						onClick={handleAddInputs}
-					/>
-					<Button
-						id="submit"
-						type="submit"
-						value="submit"
-						text="Submit"
-						onClick={handleSubmit}
-					/>
-				</form>
-			</main>
-		</>
-	);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setPage(1);
+  }
+
+  return (
+    <>
+      <Header className="title" titleText="CV Application" />
+      <main>
+        <form className="application">
+          <PersonalInformation
+            dataInfo={elementsInfo.generalInfo}
+            onChange={handleInput}
+            inputValues={inputValues}
+            onClick={handleAddInputs}
+          />
+          <Education
+            dataInfo={elementsInfo.education}
+            onChange={handleInput}
+            inputValues={inputValues}
+            onClick={handleAddInputs}
+          />
+          <Experience
+            dataInfo={elementsInfo.work}
+            onChange={handleInput}
+            inputValues={inputValues}
+            onClick={handleAddInputs}
+          />
+          <Button
+            id="submit"
+            type="submit"
+            value="submit"
+            text="Submit"
+            onClick={handleSubmit}
+          />
+        </form>
+      </main>
+    </>
+  );
 }
 
 function Header({ className, titleText }) {
-	return <h1 className={className}>{titleText}</h1>;
+  return <h1 className={className}>{titleText}</h1>;
+}
+
+function updateChildrenValues(childrenArray, id, value) {
+  let length = childrenArray.length;
+  return childrenArray.map((element, index) =>
+    index == length - 1 ? { ...element, [id]: value } : element
+  );
 }
 
 export { CvApplication, Header };
