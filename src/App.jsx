2dc5b583d37f6a0bdcc85d9df517b6641fea7c98
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-12-31 18:30:01 -0500

    (refactor) App goes back to having state.
    -> not necessary to lift stat up from App as what wasn't working is properly working now.
     - the values now when edit is clicked comeback to the input fields.

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,26 @@ import CvRendered from './components/cv-rendered';
 import { cvData } from './components/data';
 
 export default function App() {
-	const [page, setPage] = useState(0);
-	const [inputValues, setInputValues] = useState(cvData);
+  const [page, setPage] = useState(0);
+  const [inputValues, setInputValues] = useState(cvData);
 
-	return page === 0 ? (
-		<CvApplication
-			setPage={setPage}
-			inputValues={inputValues}
-			setInputValues={setInputValues}
-		/>
-	) : (
-		<CvRendered
-			form={inputValues}
-			changePage={setPage}
-			setValues={setInputValues}
-		/>
-	);
+  function handleEdit() {
+    setPage(0);
+    console.log(inputValues);
+  }
+
+  return page === 0 ? (
+    <CvApplication
+      setPage={setPage}
+      inputValues={inputValues}
+      setInputValues={setInputValues}
+    />
+  ) : (
+    <CvRendered
+      form={inputValues}
+      changePage={setPage}
+      setValues={setInputValues}
+      onEdit={handleEdit}
+    />
+  );
 }
